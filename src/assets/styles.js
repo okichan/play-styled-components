484ebe6ebed53ccd105acad8e044f8bfdba45459
Tomomi <madeinwatashi@gmail.com>
2018-03-25 00:51:32 +1100

    Add text curve

---
+++ src/assets/styles.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+
+export const Container = styled.body`
+  background: #fffcf5;
+  font-size: 1.5em;
+  color: purple;
+`;
+
+export const Title = styled.h1`
+  margin: 0;
+  padding: 0;
+  font-size: 1.5em;
+  color: purple;
+`;
+
+export const P = styled.p`
+  font-family: sans-serif;
+`;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,30 @@
 import React from "react";
 import Link from "gatsby-link";
-import styled from 'styled-components';
+import "./App.css";
+import * as S from "../assets/styles";
+import Typography from "typography";
 
-const Title = styled.h1`
-  font-size: 1.5em;
-  color: purple;
-`;
+
+const typography = new Typography({
+  baseFontSize: "18px",
+  baseLineHeight: 1.45,
+  headerFontFamily: ["Avenir Next", "Helvetica Neue", "Segoe UI", "Helvetica", "Arial", "sans-serif"],
+  bodyFontFamily: ["Arial", "serif"]
+});
 
 export default () => (
-  <div>
-    <Title>Hello Gatsby!</Title>
-    <p>What a world.</p>
-    <img src="https://source.unsplash.com/random/400x200" alt="" />
+  <S.Container>
+    <div className="svg-container">
+      <svg viewBox="0 0 500 500" preserveAspectRatio="xMidYMin slice">
+        <path id="curve" fill="transparent" d="M73.2,148.6c4-6.1,65.5-96.8,178.6-95.6c111.3,1.2,170.8,90.3,175.1,97" />
+        <text>
+          <textPath xlinkHref="#curve">&nbsp;&nbsp;&nbsp;TOMOMI</textPath>
+        </text>
+      </svg>
+    </div>
+    <S.Title>Hello Gatsby!</S.Title>
+    <S.P>What a world1121....!</S.P>
+    {/* <img src="https://source.unsplash.com/random/400x200" alt="" /> */}
     <br />
     <div>
       <Link to="/page-2/">Link</Link>
@@ -19,5 +32,21 @@ export default () => (
     <div>
       <Link to="/counter/">Counter</Link>
     </div>
-  </div>
+
+    <div id="warped">
+      <span class="w0">T</span>
+      <span class="w1">O</span>
+      <span class="w2">M</span>
+      <span class="w3">O</span>
+      <span class="w4">M</span>
+      <span class="w5">I</span>
+    </div>
+
+
+
+
+
+
+    
+  </S.Container>
 );
