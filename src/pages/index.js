d3ad432e6a1ed73961b56336d93a2c4ef8ff32e7
Tomomi <madeinwatashi@gmail.com>
2018-03-24 15:32:49 +1100

    Add styled-components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import Link from "gatsby-link";
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  color: purple;
+`;
 
 export default () => (
-  <div style={{ color: `tomato` }}>
-    <h1>Hello Gatsby!</h1>
+  <div>
+    <Title>Hello Gatsby!</Title>
     <p>What a world.</p>
     <img src="https://source.unsplash.com/random/400x200" alt="" />
     <br />
